6fbe8e56ed55a75616f3f6376c92cdadaf0aeeb4
Jordii05 <jordicl2018@gmail.com>
2025-11-14 16:52:15 -0300

    agregado de metodo put para actualizar datos

--- tp/src/main/java/com/progra/tp/controller/AgenteController.java
+++ tp/src/main/java/com/progra/tp/controller/AgenteController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.progra.tp.model.Agente;
+import com.progra.tp.model.dtos.AgenteAsignacionDTO;
 import com.progra.tp.service.interfaces.IAgenteService;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -33,5 +36,17 @@ public class AgenteController {
     public ResponseEntity<Agente> crearAgente(@RequestBody Agente agente) {
         Agente nuevoAgente = agenteService.crearAgente(agente); // guarda en Neo4j
         return ResponseEntity.ok(nuevoAgente);
+    }
+
+    @PutMapping("/{agenteId}")
+    public ResponseEntity<AgenteAsignacionDTO> actualizarAgente(
+            @PathVariable Long agenteId,
+            @RequestBody AgenteAsignacionDTO agenteActualizado) {
+        try {
+            AgenteAsignacionDTO agenteDTO = agenteService.actualizarAgente(agenteId, agenteActualizado);
+            return ResponseEntity.ok(agenteDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
