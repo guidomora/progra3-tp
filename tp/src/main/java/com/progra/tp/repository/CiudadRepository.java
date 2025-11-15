8882f0a39a266c60c561a5a37c6b4f93d52a9778
abergantino <adriel.alejandro1@outlook.com>
2025-11-15 12:55:02 -0300

    Añade consulta findByIdWithRutas para carga optimizada en neo

--- tp/src/main/java/com/progra/tp/repository/CiudadRepository.java
+++ tp/src/main/java/com/progra/tp/repository/CiudadRepository.java
@@ -3,9 +3,16 @@ package com.progra.tp.repository;
 import java.util.Optional;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import com.progra.tp.model.Ciudad;
 
 public interface CiudadRepository extends Neo4jRepository<Ciudad, Long> {
     Optional<Ciudad> findByNombre(String nombre);
+
+    @Query("MATCH p=(c:Ciudad)-[r:CONECTADA_CON*0..10]->(v) " + "WHERE id(c) = $origenId " + 
+           "RETURN c")
+
+    Optional<Ciudad> findByIdWithRutas(Long origenId);
 }
+
