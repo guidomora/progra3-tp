bf38e1031f70b6c3086f620299b1581d9c6cd4a7
Jordii05 <jordicl2018@gmail.com>
2025-11-16 23:21:29 -0300

    ahora la consulta carga por completo todo el grafo, ya no solo las rutas del nodo inicial

--- tp/src/main/java/com/progra/tp/repository/CiudadRepository.java
+++ tp/src/main/java/com/progra/tp/repository/CiudadRepository.java
@@ -1,5 +1,6 @@
 package com.progra.tp.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
@@ -11,12 +12,10 @@ public interface CiudadRepository extends Neo4jRepository<Ciudad, Long> {
     Optional<Ciudad> findByNombre(String nombre);
 
     @Query("""
-       MATCH (c:Ciudad)
-       WHERE id(c) = $origenId
-       OPTIONAL MATCH (c)-[r:CONECTADA_CON]->(d:Ciudad)
-       RETURN c, collect(r) AS rutas, collect(d) AS destinos
-       """)
-
-    Optional<Ciudad> findByIdWithRutas(Long origenId);
+    MATCH p = (c:Ciudad)-[:CONECTADA_CON*0..10]->(x:Ciudad)
+    WHERE id(c) = $origenId
+    RETURN p
+    """)
+    List<Ciudad> cargarSubgrafo(Long origenId);
 }
 
